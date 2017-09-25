a07a29d8d3ccee0a477f9c93ba238d386196ad1b
gustjr123 <hs.cho9647@gmail.com>
2017-09-25 20:58:40 +0900

    Hyeonseok cho
    Add

--- Study_Basic_Hyeonseok/Hamming_Distance.c
+++ Study_Basic_Hyeonseok/Hamming_Distance.c
@@ -13,13 +13,31 @@ int main(void)
 int HamingDistance(int x, int y)
 {
 	int count=0;
-	int i=0;
 	int num = x^y;
 	do
 	{
-		count += num & 1;
-		num = num >> 1;
+		(num & 1) ? ++count : NULL;
+		num >>= 1;
 	} while(num);
 	printf("%d\n", count);
+
+	return 0;
 }
-		
+	
+//
+//int main(void)
+//{
+//	int x,y,i;
+//	int coumt = 0;
+//	int num;
+//	scanf("%d %d", &x, &y);
+//
+//	num = x^y;
+//	for(i=1;i<32;i++)
+//	{
+//		num=num>>1;
+//		if(num%2==1)
+//			count+=1;
+//	}
+//	return 0;
+//}
